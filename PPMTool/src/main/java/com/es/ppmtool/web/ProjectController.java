502845a94bf83edc6ac0b743ab3a425db216b8de
erosai <eror.shahini@gmail.com>
2020-12-31 00:44:30 +0100

    added find all projects.

--- PPMTool/src/main/java/com/es/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/com/es/ppmtool/web/ProjectController.java
@@ -45,5 +45,8 @@ public class ProjectController {
         return new ResponseEntity<Project>(project, HttpStatus.OK);
 
     }
-
+    @GetMapping("/all")
+    public Iterable<Project> findAllProjects(){
+        return projectService.findAllProjects() ;
+    }
 }
