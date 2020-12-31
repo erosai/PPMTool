c54331b87bd09708fa6efbeb49c10ce3cb6bba93
erosai <eror.shahini@gmail.com>
2020-12-31 18:16:07 +0100

    Added some comments

--- PPMTool/src/main/java/com/es/ppmtool/domain/Project.java
+++ PPMTool/src/main/java/com/es/ppmtool/domain/Project.java
@@ -12,16 +12,24 @@ import java.util.Date;
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Generates an ID value starting with 1.
+    //@Generated Value may be applied to a primary key of an entity or a mapped superclass in conjunction with the Id annotation
+    //@Id Specifies the the primary key of an entity
     private Long id ;
     @NotBlank(message="Project Name cannot be blank")
+    //Validation type not blank , from javax.validation
     private String projectName ;
     @NotBlank(message = "Project Identifier is required")
     @Size(min = 4 ,max=4,message = "Please use 4 characters")
+    //Validation type sets the size , min = .. , max = .. , from javax.validation
     @Column(unique = true,updatable = false)
+    //Column annotation is used to specify the details of the column to which a field or property will be mapped,
+    // attributes(name , length , nullable , unique)
     private String projectIdentifier ;
     @NotBlank(message = "Project Description is required")
     private String description ;
     @JsonFormat(pattern = "yyyy-mm-dd")
+    //It is a Jackson annotation that is used to specify how to format fields/and or properties for JSON output.
     private Date start_date ;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date ;
@@ -99,11 +107,13 @@ public class Project {
     }
 
     @PrePersist
+    //Add date when entry was created
     protected void onCreate(){
         this.created_At = new Date();
     }
 
     @PreUpdate
+    //Add date when entry was updated
     protected void onUpdate(){
         this.updated_At = new Date();
     }
--- PPMTool/src/main/java/com/es/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/com/es/ppmtool/services/ProjectService.java
@@ -12,18 +12,23 @@ public class ProjectService {
 
     @Autowired
     private ProjectRepository projectRepository ;
+    //Create an instance of ProjectRepository
 
     public Project saveOrUpdateProject (Project project){
 
         try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            //Change projectIdentifier to uppercase even if the user has inputed lowercase
             return projectRepository.save(project);
+            //Saves project into project repository , using build in crud repository class.
         }catch (Exception e){
             throw new ProjectIdException("Project id '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+            // throws a ProjectIdException if projectIdentifier is already in use.
+            //Turn to uppercase fisrt since all entries are uppercase.
         }
     }
     public Project findProjectByIdentifier(String projectId){
-
+    //Find project by projectIdentifier.
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
         if(project == null){
             throw new ProjectIdException("Project ID '"+projectId.toUpperCase()+"' doesn't exist");
